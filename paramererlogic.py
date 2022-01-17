2055f6991dedf0dd86c0fb466b235948387853b2
praveen <praveenb131@gmail.com>
2022-01-18 00:34:51 +0530

    parameterlogic_added

---
+++ paramererlogic.py
@@ -0,0 +1,9 @@
+# def up_down_number(n):
+#     return n-1,n+1
+#
+# print(up_down_number(4))
+# print(up_down_number(8))
+# print(up_down_number(456))
+# print(up_down_number(6789))
+# print(up_down_number(56567468))
+
