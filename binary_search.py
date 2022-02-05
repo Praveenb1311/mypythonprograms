96d0341712f54d73259532d561c9112ea12cbfc0
praveen <praveenb131@gmail.com>
2022-02-06 00:51:53 +0530

    binary_search

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,5 @@
-list1 = [1,2,3,4,5,6,7,8,9] #in binary or dit it should be in sorting order
-key = 10
+list1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] #in binary or dit it should be in sorting order
+key = 15
 # if key in list1:
 #     print("yes!!! key found")
 # for item in list1: #linear search by 9 iteration
@@ -9,15 +9,16 @@ key = 10
 
 low = 0
 high = (len(list1)-1) #convering to 8 th index
-
+counter = 0
 while(high >= low): # check the condition before
+    counter += 1
     mid = (low + high)//2  # flooring division-//
     if list1[mid] < key:
         low = mid+1
     elif list1[mid] > key:
         high = mid - 1
     else:
-        print("key {} found at {}".format(key, mid))
+        print("key {} found at {} with {} iterations".format(key, mid, counter))
         break
 
 
