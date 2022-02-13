13cc67c0670e7bc640672f01bd3931b9ac4dc07a
praveen <praveenb131@gmail.com>
2022-02-14 00:30:01 +0530

    car_optimization

--- car_optimization.py
+++ car_optimization.py
@@ -3,3 +3,16 @@
 P = [2,3,1,5]
 S = [4,2,5,3]
 
+
+#group of faactories- number of produce polution in units - to find minimize fiter - one filter will reduce half of the polution
+
+
+F = [3,5,19,8]
+
+output = 3
+
+#19-2,  8-1,
+
+F = [35]
+O = 17
+
