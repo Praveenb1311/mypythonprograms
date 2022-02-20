2c33f630f38f4bc735fcfbe98c26674dd25ed252
praveen <praveenb131@gmail.com>
2022-02-21 00:15:40 +0530

    find_the_area

--- find_the_area.py
+++ find_the_area.py
@@ -14,6 +14,33 @@ s = (a + b + c) / 2
 
 # calculate the area
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+print('The area of the triangle is : {:.2f}'.format(area))
 
-PI = 3.14
+# Area of circle
+PI = 3.1416
+r = 7
+area = PI * r * r
+print('The area of the circle is : {:.2f}'.format(area))
+
+# Area of rectangle
+
+a = 5
+b = 6
+
+area = a * b
+print('The area of the rectangle is : {:.2f}'.format(area))
+
+
+# Area of cylinder
+
+PI = 3.1416
+r = 7
+h = 8
+area1 = 2* PI * r * r
+area2 = 2* PI * r * h
+area = area1 + area2
+print('The area of the cylinder is : {:.2f}'.format(area))
+
+
+# square
+
