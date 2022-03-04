ef225ecdab46577b0aec6ed1ef7c9fdda77b3492
praveen <praveenb131@gmail.com>
2022-03-05 00:34:50 +0530

    matrix_mukti1

---
+++ matrix_multiplication.py
@@ -0,0 +1,75 @@
+#
+# def Principal_Diagonal(matrix, n):
+#     print("Principal Diagonal: ", end="")
+#     diagonal_element =[]
+#     for i in range(n):
+#      for j in range(n):
+#         if (i == j):
+#             diagonal_element.append(matrix[i][j])
+#     return diagonal_element
+#
+#
+# def Secondary_Diagonal(matrix, n):
+#     print("Secondary Diagonal: ", end="")
+#     sec_diagonal_element = []
+#     for i in range(n):
+#         for j in range(n):
+#             if ((i + j) == (n - 1)):
+#                 sec_diagonal_element.append(matrix[i][j])
+#
+#     return sec_diagonal_element
+
+
+
+# n = 4
+#
+# a = [[1, 2, 3, 4],
+#
+#      [5, 6, 8, 8],
+#
+#      [1, 2, 3, 4],
+#
+#      [5, 6, 7, 8]]
+#
+# diagonal_element = Principal_Diagonal(a,n)
+# sec_diagonal_element = Secondary_Diagonal(a,n)
+# # print(abs(sum(diagonal_element)-sum(sec_diagonal_element)))
+#
+# A = [2,3,3,4,5,13,14,11]
+# # 3 = 55
+# # 3 = 60
+# 5 =70
+# 10 = 80
+# # 3 = 45
+# # 11 = 90
+# total_sum = 0
+# for i in range(len(A)):
+#     n,m  = input().split()
+#     n,m = int(n),int(m)
+#     if n in A:
+#         total_sum += m
+#         A.remove(n)
+# print(total_sum)
+#
+
+# huli 10 90 99
+# praveen 15 4 25
+# gauda 4 25 85
+# gouda
+n = 3
+students = []
+for i in range(n):
+    name,m1,m2,m3 = input().split()
+    students.append([name,m1,m2,m3])
+name = input("enter the name")
+for student in students:
+    if student[0] == name:
+        average = (int(student[1])+int(student[2])+int(student[3]))/3
+        print("{:.2f}".format(average))
+
+        break
+
+
+Assignment:
+
+
--- strict_superset.py
+++ strict_superset.py
@@ -1,8 +1,8 @@
-A = {1,2,3,4,5}
-B = {1,2,3}
-C = {1,2,4}
-D = {1,2,3,4,5}
-E ={4,5,6}
+# A = {1,2,3,4,5}
+# B = {1,2,3}
+# C = {1,2,4}
+# D = {1,2,3,4,5}
+# E ={4,5,6}
 
 #
 #
@@ -23,29 +23,38 @@ E ={4,5,6}
 
 
 
-
-def given_set(A,B):
-    if len(A - B) > 0:
-        print("A is strict_supeset of B")
-
-    elif len(A - B) == 0:
-        print("A is not strict_supeset of B")
-
-
-    elif len(B - A) > 0:
-        print("A is not strict_supeset of B")
-
-given_set({1,2,3,4}, {1,2,3,4})
-given_set({1,2,3}, {1,2,3,4})
-given_set({1,2,3,4}, {1,2,3})
-given_set({4,2,3}, {1,2,3,4})
-<<<<<<< HEAD
-given_set({1,2,3,4,5}, {4,2,1,3})
+#
+# def given_set(A,B):
+#     if len(A - B) > 0:
+#         print("A is strict_supeset of B")
+#
+#     elif len(A - B) == 0:
+#         print("A is not strict_supeset of B")
+#
+#
+#     elif len(B - A) > 0:
+#         print("A is not strict_supeset of B")
+#
+# given_set({1,2,3,4}, {1,2,3,4})
+# given_set({1,2,3}, {1,2,3,4})
+# given_set({1,2,3,4}, {1,2,3})
+# given_set({4,2,3}, {1,2,3,4})
+# <<<<<<< HEAD
+# given_set({1,2,3,4,5}, {4,2,1,3})
 
 ## principle diagonal element- secondary diagonal element
 
-1,2
-3,3
-=======
-given_set({1,2,3,4,5}, {4,2,1,3})
->>>>>>> origin/main
+A = [[1,2],[3,4]]
+B = [[1,2],[3,4]]
+
+
+
+set1 = [((A([0][0] * A([1][1]))-A([0][1] * A([1][0]))]
+
+print(set1)
+
+
+C = [[1,2],[3,4],[5,6]]
+D = [[1,2],[3,4],[5,6]]
+
+
