e39ad498de8141e1540c92451d34cd37617988d9
praveen <praveenb131@gmail.com>
2022-06-22 22:10:18 +0530

    tower_of_hannoy

--- tower_of_anoy.py
+++ tower_of_anoy.py
@@ -12,5 +12,38 @@ disks = int(input('Enter number of disks: '))
 hanoi(disks, 'A', 'B', 'C')
 print(2**disks-1)
 
-00001111
-00000001
+
+# Recursive function to solve Tower
+# of Hanoi puzzle
+def towerOfHanoi(n, from_rod, to_rod, aux_rod1,
+aux_rod2):
+
+if (n == 0):
+return
+if (n == 1):
+print(“Move disk”, n, “from rod”,
+from_rod, “c to rod”, to_rod)
+return
+
+towerOfHanoi(n – 2, from_rod, aux_rod1,
+aux_rod2, to_rod)
+print(“Move disk”, n-1, “from rod”, from_rod,
+“c to rod”, aux_rod2)
+
+
+print(“Move disk”, n, “from rod”, from_rod,
+“c to rod”, to_rod)
+
+print(“Move disk”, n-1, “from rod”, aux_rod2,
+“c to rod”, to_rod)
+
+towerOfHanoi(n – 2, aux_rod1, to_rod,
+from_rod, aux_rod2)
+
+# driver program
+n = 4 # Number of disks
+
+# A, B, C and D are names of rods
+towerOfHanoi(n, ‘A’, ‘D’, ‘B’, ‘C’)
+
+# This code is contributed by Smitha.
