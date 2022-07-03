c5e457291595eaa45205c47b41eb4beeed69a513
praveen <praveenb131@gmail.com>
2022-07-03 23:30:35 +0530

    praveen_practice1.py

---
+++ praveen_practice1.py
@@ -0,0 +1,18 @@
+# insertion sort:
+
+list1 = [11,6,35,8,13,95,18]
+
+# step1: 6 we need to keep it as constant or reference element,
+# 6,11,35,8,13,95,18
+#step2: 35 we need to keep it as constant or reference element,
+# 6,11,35,8,13,95,18
+#step3: 8 we need to keep it as constant or reference element,
+# 6,8,11,35,13,95,18
+#step4: 13 we need to keep it as constant or reference element,
+# 6,8,11,13,35,95,18
+#step5: 95 we need to keep it as constant or reference element,
+# 6,8,11,13,35,95,18
+#step6: 18 we need to keep it as constant or reference element,
+# 6,8,11,13,18,35,95
+
+# merge_sort:
