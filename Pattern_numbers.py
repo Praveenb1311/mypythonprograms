4fb8a3a241aaf0c16c4c77dc7715331515af7259
Praveenb1311 <praveenb1311@gmail.com>
2022-12-14 23:13:11 +0530

    hollow right angle

--- Pattern_numbers.py
+++ Pattern_numbers.py
@@ -10,3 +10,13 @@ for i in range(1, n+1):
     for j in range(1, i+1):
         print(i, end = "")
     print()
+
+
+n = int(input("Please enter the number of rows: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == (n-1) or row == col:
+            print("*", end ="")
+        else:
+            print(end = " ")
+    print()
