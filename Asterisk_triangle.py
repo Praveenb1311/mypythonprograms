86363e5d175fe2fdc1b16238defca2f91fe833cf
Praveenb1311 <Praveenb1311@GMAIL.COM>
2023-06-06 07:14:27 +0530

    Added Asterisk program

---
+++ Asterisk_triangle.py
@@ -0,0 +1,19 @@
+# def pyfunc(r):
+#     for x in range(r):
+#         print(""*(r-x-1)+"*"*(2*x+1))
+# pyfunc(4)
+
+# def pyfunc(r):
+#     for x in range(r):
+#         print(""*(r-x-1)+"*"*(x+1))
+# pyfunc(4)
+
+def pyfunc(r):
+    for x in range(r):
+        print(""*(r-x-1)+"*"*(3*x+1))
+pyfunc(4)
+
+# def pyfunc(r):
+#     for x in range(r):
+#         print(""*(r-x-1))
+# pyfunc(4)
